c17e706dbb6136da831194b0606e4da77a7424da
ADITYA SAHU <adizz2407@gmail.com>
2023-04-29 14:24:49 -0700

    Updated preprocessing_market_bias.py

--- notebooks/dataset_preparation/MarketBais/preprocessing_market_bias.py
+++ notebooks/dataset_preparation/MarketBais/preprocessing_market_bias.py
@@ -4,7 +4,7 @@ import numpy as np
 import time
 from datetime import datetime
 
-df= pd.read_csv('C:/Users/adity/Desktop/SJSU_work/Sem2/ADM/PROJECT/group-recommender-master - adi/drgr/df_electronics.csv')
+df= pd.read_csv('C:/Users/adity/Desktop/SJSU_work/Sem2/ADM/PROJECT/group-recommender-master_adi/drgr/df_electronics.csv')
 df=df.drop(['model_attr','category','brand','year','user_attr','split'],axis=1)
 df['rating']=df['rating'].astype(int)
 df = df[['user_id', 'item_id','rating','timestamp']]
@@ -15,18 +15,19 @@ for i in range(len(df['timestamp'])):
   timestamp = int(date_obj.timestamp())
   df['timestamp'][i]=timestamp
 
-#store in csv
-df.to_csv('market_preprocessing.csv')
+df_20k = df[1:20001]
+# store in csv
+df_20k.to_csv('market_preprocessing_20k.csv')
+df_20k['user_id'].to_csv('item_data.csv', index=False, header=True)
+df_20k['user_id'].to_csv('users_data.csv', index=False, header=True)
 
-#store in .dat 
-output_file = 'market_preprocessing.dat'
+# store in .dat
+output_file = 'market_preprocessing_20k.dat'
 
 # Open the output file for writing
 with open(output_file, 'w') as f:
-    
     # Iterate over each row of the dataframe
-    for index, row in df.iterrows():
-        
+    for index, row in df_20k.iterrows():
         # Write the data to the output file in the desired format
         f.write('{}::{}::{}::{}\n'.format(row['user_id'], row['item_id'], row['rating'], row['timestamp']))
 print("done")
